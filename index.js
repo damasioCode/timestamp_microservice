88ab4193acea5232c352dc8a11fa424b8f4d32f0
Daniel <damasio.dan7@gmail.com>
2022-07-08 19:42:14 -0300

    feature: timestamp route working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 // where your node app starts
+require('dotenv').config()
 
 // init project
 var express = require('express');
@@ -18,13 +19,27 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:timestamp", function(req, res){
+  const {timestamp} = req.params;
 
+  const data = new Date(timestamp*1000);
+  console.log(data);
+  if(data != "Invalid Date") {
+    res.status(201).json({
+      "unix": Number(timestamp) , 
+      "utc": data.toUTCString()
+    });
+    return;
+  }
+  res.status(400).json({ 
+    error : "Invalid Date" 
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
